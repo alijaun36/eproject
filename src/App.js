a76cc323fc44c16772980f3939df2a7b3673bd3a
alijaun36 <47271962+alijaun36@users.noreply.github.com>
2023-02-15 14:33:51 +0500

    eproject

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          { <h1>Welcome To ProShop</h1> }
+          <Routes>
+            <Route path='/' element={<HomeScreen />}></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,9 @@
+function ProductScreen() {
+    return ( 
+        <div>
+            Product Screen
+        </div>
+     );
+}
+
+export default ProductScreen
